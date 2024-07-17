ed4b3d047fca6e07e56d138e70b2d54587bed2a2
kangxx <kangxx@sany.com.cn>
2024-07-17 14:48:59 +0800

    feat: 更新代码

--- js/formatFloat.js
+++ js/formatFloat.js
@@ -4,4 +4,7 @@ const formatFloat = (num, bitNum = 2) => {
   return Math.round(num * retainNum) / retainNum;
 };
 
+
+let reg = new RegExp(/^[0-9]+(.[0-9]{1,2})?$/); // 正则表达式 校验是否两位小数
+console.log(reg.test(123.55211));
 console.log(formatFloat(123.55211, 1));
